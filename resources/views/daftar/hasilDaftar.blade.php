e6ebb905180fcacce0c5017ea9f460f87a3f2d92
jossman <jossman14@github.com>
2020-12-03 10:41:54 +0700

    updaet branh

--- resources/views/daftar/hasilDaftar.blade.php
+++ resources/views/daftar/hasilDaftar.blade.php
@@ -205,7 +205,7 @@ $("#btn-Convert-Html2Image").on('click', function() {
 var imgageData =
 getCanvas.toDataURL("image/png");
 
-//test pull
+//test 
 // Now browser starts downloading
 // it instead of just showing it
 var newData = imgageData.replace(
